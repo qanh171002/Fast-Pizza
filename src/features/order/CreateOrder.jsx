83ce717716f8ce964d6099a1a24321a3efa73a88
Anh <qanh171002@gmail.com>
2024-12-29 04:40:13 +0700

    style

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -48,13 +49,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -62,12 +63,13 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
         <div>
           <input
+            className="w-6 h-6 outline-none accent-yellow-400 focus:ring-1 focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
@@ -79,9 +81,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <Button disabled={isSubmitting}>
             {isSubmitting ? "Placing order...." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -17,6 +17,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="px-4 py-2 text-sm transition-all duration-300 bg-yellow-100 rounded-full outline-none placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 focus:ring-1 focus:ring-yellow-500 focus:ring-opacity-50"
       />
     </form>
   );
