c8392d89b63a768d57599a026a201905b317e43a
Anh <qanh171002@gmail.com>
2024-12-28 14:26:23 +0700

    search order

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
+
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
-
     children: [
       {
         path: "/",
@@ -25,7 +25,12 @@ const router = createBrowserRouter([
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
