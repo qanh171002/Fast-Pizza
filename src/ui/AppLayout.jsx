21cf5a2e1a5cafd972b10d6ec72a60f65fc5196a
Anh <qanh171002@gmail.com>
2024-12-29 22:52:18 +0700

    hot fix

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,26 +1,26 @@
-import Header from "./Header";
-import Loader from "./Loader";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import Header from './Header'
+import Loader from './Loader'
+import CartOverview from '../features/cart/CartOverview'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 function AppLayout() {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
-  return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {isLoading && <Loader />}
+    const navigation = useNavigation()
+    const isLoading = navigation.state === 'loading'
+    return (
+        <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+            {isLoading && <Loader />}
 
-      <Header />
+            <Header />
 
-      <div className="overflow-auto">
-        <main className="max-w-3xl mx-auto ">
-          <Outlet />
-        </main>
-      </div>
+            <div className="overflow-auto">
+                <main className="mx-auto max-w-3xl">
+                    <Outlet />
+                </main>
+            </div>
 
-      <CartOverview />
-    </div>
-  );
+            <CartOverview />
+        </div>
+    )
 }
 
-export default AppLayout;
+export default AppLayout
