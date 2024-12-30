16ecfc8486a53e07f77b96370d6fd58675d2bb30
Anh <qanh171002@gmail.com>
2024-12-30 22:16:25 +0700

    cart global state, add delete item from cart

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,13 +1,20 @@
-import { Link } from 'react-router-dom';
+import LinkButton from '../../ui/LinkButton'
 
 function EmptyCart() {
-  return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
-
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
-    </div>
-  );
+    return (
+        <div className="px-4 py-3">
+            <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+            <div className="mt-7 flex flex-col items-center justify-center">
+                <img
+                    className="h-20 w-20"
+                    src="https://cdn-icons-png.flaticon.com/512/17569/17569003.png"
+                ></img>
+                <p className="mt-7 text-center font-semibold">
+                    Your cart is still empty. Start adding some pizzas.
+                </p>
+            </div>
+        </div>
+    )
 }
 
-export default EmptyCart;
+export default EmptyCart
