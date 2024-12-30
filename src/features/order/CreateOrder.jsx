0c9108d958c9f39158b7ee5f759e86a97499b57c
Anh <qanh171002@gmail.com>
2024-12-30 16:10:14 +0700

    user global state

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 import Button from '../../ui/Button'
+import { useSelector } from 'react-redux'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -35,6 +36,8 @@ const fakeCart = [
 
 function CreateOrder() {
     // const [withPriority, setWithPriority] = useState(false);
+    const username = useSelector((state) => state.user.username)
+
     const navigation = useNavigation()
     const isSubmitting = navigation.state === 'submitting'
 
@@ -55,6 +58,7 @@ function CreateOrder() {
                         className="input grow"
                         type="text"
                         name="customer"
+                        defaultValue={username}
                         required
                     />
                 </div>
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,18 +1,34 @@
-import CreateUser from "../features/user/CreateUser";
+import { useSelector } from 'react-redux'
+import CreateUser from '../features/user/CreateUser'
+import Button from './Button'
 
 function Home() {
-  return (
-    <div className="px-4 my-10 text-center sm:my-16">
-      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
-        The best pizza.
-        <br />
-        <span className="text-yellow-500">
-          Straight out of the oven, straight to you.
-        </span>
-      </h1>
-      <CreateUser />
-    </div>
-  );
+    const username = useSelector((state) => state.user.username)
+    return (
+        <div className="my-10 px-4 text-center sm:my-16">
+            <h1 className="mb-8 text-xl font-semibold md:text-3xl">
+                The best pizza.
+                <br />
+                <span className="text-yellow-500">
+                    Straight out of the oven, straight to you.
+                </span>
+            </h1>
+            {username === '' ? (
+                <CreateUser />
+            ) : (
+                <div className="flex flex-col items-center justify-center">
+                    <img
+                        className="mb-16 h-40 w-40"
+                        src="https://cdn-icons-png.flaticon.com/512/17845/17845842.png"
+                        alt="pizza"
+                    />
+                    <Button to="/menu" type="primary">
+                        Go to our Menu, {username}!
+                    </Button>
+                </div>
+            )}
+        </div>
+    )
 }
 
-export default Home;
+export default Home
